eecb4f3c284e449339cce19f756a2c932b67dcee
ganeshgawade8708 <ganeshgawade8708@gmail.com>
2026-09-19 20:09:37 +0530

    Fix duplicate PORT declaration

--- server.js
+++ server.js
@@ -80,8 +80,6 @@ app.get('/api/orders/:userId', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
